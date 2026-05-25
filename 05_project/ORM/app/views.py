9fc9aff79d14f5c6bb7e280d1c86cda0baa23035
Ashok-12112 <ashoknayak55899@gmail.com>
2026-05-25 15:39:44 +0530

    first commit

--- 05_project/ORM/app/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Student
-students = Student.objects.all()
-
-
-def home(request):
-    return render(request,'home.html',{"student":students})
-
--- 06_project/admin_pannel/app/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from app.models import Student
-
-admin.site.register(Student)
